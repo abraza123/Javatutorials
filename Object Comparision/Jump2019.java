c0a94963bcf91214628b08d9280158a1bb593837
abraza123 <53875752+abraza123@users.noreply.github.com>
2019-08-14 08:48:34 -0400

    Add files via upload
    This is an example on how object comparison works in Java

---
+++ Object Comparision/Jump2019.java
@@ -0,0 +1,27 @@
+
+
+public class Jump2019{
+
+public static void main(String[] args) {
+
+    String jump = new String ("London");
+    String jump2 = new String ("London");
+
+
+    if (jump == jump2) {
+
+        System.out.println("Congrats!! You made it");  
+    }
+
+    else {
+        System.out.println("oops bad luck try again");
+
+    }
+
+
+}
+
+
+
+
+}
