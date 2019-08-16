b372e1ae81169072f1360d48aef540f9c20d410a
abraza123 <53875752+abraza123@users.noreply.github.com>
2019-08-16 09:28:51 -0400

    Assessment1
    This is the coding challenge of the assesment

---
+++ Assessment1/Assessment1.java
@@ -0,0 +1,58 @@
+
+
+interface Abdul{
+
+    static int val = 3;
+    static String var = "Hola";
+}
+
+
+public class Assessment1 extends Abdul{
+
+
+   static byte val2 = 2.75;
+
+    public class MyClass extends Assessment1{
+
+
+        byte total = val2 + val ;
+
+
+
+
+
+
+
+        public class HomeRoom extends MyClass{
+
+            String var2 = "Hello";
+
+            Abdul(){
+
+                System.out.println(total);
+            }
+
+
+            public class Jump extends MyClass{
+
+                
+                
+                
+            }
+        
+        }
+
+
+
+    }
+
+
+
+
+    
+    
+
+
+
+
+}
